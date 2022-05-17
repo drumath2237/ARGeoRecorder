c4ef4d36422e8b929301d6d589a140104ed581ce
drumath <2237k.drumath@gmail.com>
2022-05-17 19:33:04 +0900

    :sparkles: recording処理WIP

--- Assets/ARRecoreder/Scripts/ARSessionRecorder.cs
+++ Assets/ARRecoreder/Scripts/ARSessionRecorder.cs
@@ -1,13 +1,25 @@
 ﻿using System;
+using System.IO;
 using UnityEngine;
-using UnityEngine.XR.ARCore;
 using UnityEngine.XR.ARFoundation;
+
+#if UNITY_ANDROID
+using UnityEngine.XR.ARCore;
+#endif
+
 
 namespace ARRecorder
 {
     public class ARSessionRecorder : MonoBehaviour
     {
         [SerializeField] private ARSession arSession;
+
+        private string mp4path = null;
+
+        private void Awake()
+        {
+            mp4path = Path.Combine(Application.persistentDataPath, "arcore-session.mp4");
+        }
 
         public void OnStartRecord()
         {
@@ -16,6 +28,24 @@ namespace ARRecorder
             {
                 return;
             }
+
+
+            var isPlayingOrRecording = subsystem.playbackStatus.Playing() || subsystem.recordingStatus.Recording();
+            if (isPlayingOrRecording)
+            {
+                // todo: recording停止処理を淹れてもいいかも
+                return;
+            }
+
+            var finishedPlayingAndRecording = subsystem.playbackStatus == ArPlaybackStatus.Finished &&
+                                              subsystem.recordingStatus == ArRecordingStatus.None;
+            if (!finishedPlayingAndRecording)
+            {
+                return;
+            }
+
+            using var recordingConfig = new ArRecordingConfig(subsystem.session);
+            recordingConfig.SetMp4DatasetFilePath(subsystem.session, mp4path);
 
 
 #endif
