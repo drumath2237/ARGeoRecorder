0246cd305ecb72a48267ee8d042dd0e5bc25058f
drumath <2237k.drumath@gmail.com>
2022-05-17 19:10:37 +0900

    :sparkles: 簡単なスクリプトの作成

---
+++ Assets/ARRecoreder/Scripts/ARSessionRecorder.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.XR.ARCore;
+using UnityEngine.XR.ARFoundation;
+
+namespace ARRecorder
+{
+    public class ARSessionRecorder : MonoBehaviour
+    {
+        [SerializeField] private ARSession arSession;
+
+        public void OnStartRecord()
+        {
+#if UNITY_ANDROID
+            if (arSession.subsystem is not ARCoreSessionSubsystem subsystem)
+            {
+                return;
+            }
+
+
+#endif
+        }
+    }
+}
